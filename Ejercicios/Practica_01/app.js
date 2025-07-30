9500e317c754f784d7b25bf2df9b760f968afb73
Edson-Moreno1 <edson.morenoortega@gmail.com>
2025-07-30 22:17:28 +0000

    Segunda avance

--- Ejercicios/Practica_01/app.js
+++ Ejercicios/Practica_01/app.js
@@ -1,2 +1,60 @@
-alert ('Hola Mundo');
-let numeroDeUsuario = prompt ("Me indicas un número por favor:");
+//let numeroSecreto=6;
+//let numeroUsuario = prompt ("Me indicas un número por favor:");
+
+
+//console.log(numeroUsuario);
+
+
+//if (numeroUsuario == numeroSecreto){
+   // alert('Acertaste el número');
+//}
+
+//let mensajeDeBienvenida = 'Bienvenido a la aplicación';
+//alert(mensajeDeBienvenida);
+
+//alert('Bienvenidos al juego del número secreto');
+//let eleccion = prompt('Elige un número entre 1 y 10');
+//let numeroSecreto = Math.floor(Math.random()*10)+1;
+//console.log(numeroSecreto);
+//if (eleccion == numeroSecreto) {
+  //  alert('Adivinaste'); }
+
+
+//let contraseniaDelSistema = "contraseniaPrueba!";
+
+//let contrasenia = prompt("Ingrese la contraseña del sistema:");
+
+//if (contrasenia == contraseniaDelSistema){
+  //  alert("Acceso al sistema garantizado");
+//}
+
+//DESAFIO:HORA DE PRACTICAR
+
+alert ("Bienvenida y bienvenido a nuestro sitio web!")
+
+let nombre="Lua";
+
+let edad=25;
+
+let numeroDeVentas=50;
+
+let saldoDisponible=1000;
+
+// Punto 6
+alert("¡Error! Completa todos los campos");
+
+// Punto 7  
+let mensajeDeError = "¡Error! Completa todos los campos"
+alert(mensajeDeError);
+
+
+let nombreUsuario = prompt ("Favor de proporcionar el nombre del usuario")
+
+console.log(nombreUsuario);
+
+let edadUsuario= prompt("Favor de ingresar la edad");
+console.log(edadUsuario);
+if(edadUsuario >= 18 ){
+  alert("¡Puedes obtener tu licencia de conducir!");
+
+}
