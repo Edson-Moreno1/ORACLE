053d1c9920e0379b6187ece80995d831441a5252
Edson-Moreno1 <edson.morenoortega@gmail.com>
2025-08-04 22:31:33 +0000

    Commit

--- Ejercicios/Practica_02/app.js
+++ Ejercicios/Practica_02/app.js
@@ -1,10 +1,26 @@
-let titulo =document.querySelector('h1');
-titulo.innerHTML = 'Juego del número secreto';
+let numeroSecreto = generarNumeroSecreto();
 
-let parrafo = document.querySelector('p');
-parrafo.innerHTML = 'Indica un número del 1 al 10';
 
-function intentoDeUsuario(){
-    alert('click desde el boton');
+
+function asignarTextoElemento(elemento,texto){
+    let elementoHTML =document.querySelector(elemento);
+elementoHTML.innerHTML = texto;
 }
 
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(typeof(numeroDeUsuario));
+    console.log(typeof(numeroSecreto));
+    console.log(numeroSecreto);
+    console.log(numeroDeUsuario);
+    console.log(numeroDeUsuario === numeroSecreto);
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor (Math.random()*10)+1;
+    return numeroSecreto;
+}
+
+asignarTextoElemento('h1','Juego del número secreto!');
+asignarTextoElemento('p','Indica un númeroo del 1 a al 10');
