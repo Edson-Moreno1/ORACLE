de321e50645f1b7a77a3238767281d4d8bd27323
Edson-Moreno1 <edson.morenoortega@gmail.com>
2025-08-04 20:27:37 -0600

    3er commit

--- Ejercicios/Practica_02/app.js
+++ Ejercicios/Practica_02/app.js
@@ -1,5 +1,5 @@
-let numeroSecreto = generarNumeroSecreto();
-let intentos = 1;
+let numeroSecreto = 0;
+let intentos =0;
 
 
 
@@ -14,16 +14,18 @@ function verificarIntento(){
     console.log(intentos);
     if (numeroDeUsuario === numeroSecreto){
         asignarTextoElemento('p',`Crack,Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez':'veces' }`);
+        document.getElementById('reiniciar').removeAttribute ('disabled');
     }else {
 
         //El usuario no acertó
         if(numeroDeUsuario > numeroSecreto){
-            asignarTextoElemento('p','El número secretro es menor');
+            asignarTextoElemento('p','El número secreto es menor');
         }else{
             asignarTextoElemento('p','El número secreto es mayor');
         }
 
         intentos ++;
+        limpiarCaja();
     }
 
     return;
@@ -31,7 +33,8 @@ function verificarIntento(){
 
 
 function limpiarCaja(){
-    
+    document.querySelector('#valorUsuario').value = '';
+  
 }
 
 
@@ -41,6 +44,24 @@ function generarNumeroSecreto() {
     return Math.floor (Math.random()*10)+1;
     return numeroSecreto;
 }
+function condicionesIniciales(){
+    asignarTextoElemento('h1','Juego del número secreto!');
+    asignarTextoElemento('p','Indica un númeroo del 1 a al 10');
+    numeroSecreto = generarNumeroSecreto();
+    intentos =1;
+}
 
-asignarTextoElemento('h1','Juego del número secreto!');
-asignarTextoElemento('p','Indica un númeroo del 1 a al 10');
+function reiniciarJuego() {
+    //Limpiar la caja 
+    limpiarCaja();
+    //Indicar mensaje de intervalo de numero
+    //Generar el numero aleatorio
+    //Inicializar el numero intentos
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+
+    
+}
+
+condicionesIniciales();
